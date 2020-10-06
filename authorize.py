e013cd9ca2d957a21d1982b72b989068fa7d30aa
Duane Stanford <tstanford@beverage-digest.com>
2020-10-05 20:18:43 -0500

    Removed Debug Statements

--- authorize.py
+++ authorize.py
@@ -7,10 +7,8 @@ headers = {'Content-Type': 'application/json',}
 my_email = str(os.getenv('EMAIL'))
 my_password = str(os.getenv('PASSWORD'))
 
-print(my_email)
+response = requests.post('https://www.beverage-digest.com/query', json={'query':'mutation {tokenCreate(email: \"' + my_email + '\",password: \"' + my_password + '\",admin: true){ token expiresAt } }'})
 
-# response = requests.post('https://www.beverage-digest.com/query', json={'query':'mutation {tokenCreate(email: \"' + my_email + '\",password: \"' + my_password + '\",admin: true){ token expiresAt } }'})
+json_response = response.json()
 
-# json_response = response.json()
-
-# token = f"token {json_response['data']['tokenCreate']['token']}"
+token = f"token {json_response['data']['tokenCreate']['token']}"
