5f9c84100622a295c4d5aa3aa02c0ac35bc09273
Duane Stanford <tstanford@beverage-digest.com>
2020-10-05 20:13:57 -0500

    Uploading with print statements for debugging

--- authorize.py
+++ authorize.py
@@ -7,10 +7,10 @@ headers = {'Content-Type': 'application/json',}
 my_email = str(os.getenv('EMAIL'))
 my_password = str(os.getenv('PASSWORD'))
 
-response = requests.post('https://www.beverage-digest.com/query', json={'query':'mutation {tokenCreate(email: \"' + my_email + '\",password: \"' + my_password + '\",admin: true){ token expiresAt } }'})
+print(my_email)
 
-json_response = response.json()
+# response = requests.post('https://www.beverage-digest.com/query', json={'query':'mutation {tokenCreate(email: \"' + my_email + '\",password: \"' + my_password + '\",admin: true){ token expiresAt } }'})
 
-print(json_response)
+# json_response = response.json()
 
 # token = f"token {json_response['data']['tokenCreate']['token']}"
