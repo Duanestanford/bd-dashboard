09d9a340ac2cfd4d622b5613b7a74cae3161e728
Duane Stanford <duanestanford@Surf-Rock-io.local>
2020-10-27 09:13:58 -0500

    Changed epub query to remove inactive users.

--- query.py
+++ query.py
@@ -7,7 +7,7 @@ def post_request():
     body = {'query':  'query {_subscriptionStats {count}}'}
 
     data_to_send = {
-        'query': 'query {_subscriptionStats {count}}'}
+        'query': 'query {_subscriptionStats (filterString: \"status = ACTIVE\") {count}}'}
 
     url = 'https://www.beverage-digest.com/query'
 
@@ -18,8 +18,8 @@ def post_request():
 
     json_data = response.json()
 
-    sub_count_with_free = json_data['data']['_subscriptionStats']['count']
+    sub_count_active_only = json_data['data']['_subscriptionStats']['count']
 
-    sub_count = sub_count_with_free - 200
+    sub_count = sub_count_active_only
 
     return sub_count
